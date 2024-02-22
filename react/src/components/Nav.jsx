8311ac0192f8c6dc1b4795b2a51eb27460da21f8
Christian Albinsson <christian.k.albinsson@hiof.no>
2024-02-22 16:45:05 +0100

    Update Nav.jsx

--- react/src/components/Nav.jsx
+++ react/src/components/Nav.jsx
@@ -1,32 +1,28 @@
-import React from "react"
-import {useState} from "react"
-import {resources} from "../assets/ressurser.js"
+import React from "react";
+import { useState } from "react";
+import { resources } from "../assets/ressurser.js";
 
 export default function Nav() {
+  const [activeTag, setActiveTag] = useState(null);
 
-const [activeTag, setActiveTag] = useState(null)
-
-const uniqueTags = [];
-resources.forEach(category => {
+  const uniqueTags = [];
+  resources.forEach((category) => {
     if (uniqueTags.indexOf(category.category) === -1) {
-        uniqueTags.push(category.category)
+      uniqueTags.push(category.category);
     }
-});
+  });
 
-return (
-        <ul>
-            {uniqueTags.map(tag => (
-            <button 
-                key={tag}
-                className={activeTag === tag ? 'active' : ''}
-                onClick={() => setActiveTag(tag)}              
-            >
-                <a href={`${tag}`}>{tag}</a>
-            </button>
-        ))}  
-
-        </ul>
-    );
-    }
-
-
+  return (
+    <ul>
+      {uniqueTags.map((tag) => (
+        <button
+          key={tag}
+          className={activeTag === tag ? "active" : ""}
+          onClick={() => setActiveTag(tag)}
+        >
+          <a href={`#${tag}`}>{tag}</a>
+        </button>
+      ))}
+    </ul>
+  );
+}
