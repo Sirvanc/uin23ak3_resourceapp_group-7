f349130d69f977c6f6e518a78eaca5592444c4d7
Christian Albinsson <christian.k.albinsson@hiof.no>
2024-02-22 14:44:06 +0100

    Lagd nav component
    lagd nav component, med dynamiske verdier fra resurser.js

--- react/src/components/Main.jsx
+++ react/src/components/Main.jsx
@@ -1,6 +1,7 @@
 export default function Main() {
     return (
        <>
+       <h2>Main</h2>
        
        </>
     )
--- react/src/components/Nav.jsx
+++ react/src/components/Nav.jsx
@@ -1,7 +1,24 @@
+import React from "react";
+import {resources} from "../assets/ressurser.js"
+
 export default function Nav() {
-    return (
-        <>
-        
-        </>
-    )
-}
+
+const navCategory = resources.filter(category => category.category)
+console.log(navCategory)
+
+const uniqueTags = [];
+resources.forEach(category => {
+    if (uniqueTags.indexOf(category.category) === -1) {
+        uniqueTags.push(category.category);
+    }
+});
+
+return (
+    <> 
+        <ul>
+            {uniqueTags.map(tag => <li key={tag}><a href={`#${tag}`}>{tag}</a></li>)}      
+        </ul>
+    </>
+    );
+    }
+
