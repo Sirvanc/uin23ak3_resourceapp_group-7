acd196bf82dd762faf2e7cde016793ce598fad30
Sirvan Calabi <88987521+Sirvanc@users.noreply.github.com>
2024-02-23 02:21:35 +0100

    Update Main

--- react/src/components/Main.jsx
+++ react/src/components/Main.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 
-export default function Main(props) {
-  const { activeTab } = props;
-  const uppercaseActiveTab = activeTab.toUpperCase();
-  return (
-    <>
-      <h1>{uppercaseActiveTab}</h1>
-    </>
-  );
+export default function Main() {
+    return (
+        <div className='ressouceBody'>
+            <h2>Ressursarkiv </h2>
+            <nav>
+        <ul>
+        <li><a href="/html">HTML</a></li>
+        <li><a href="/css">CSS</a></li>
+        <li><a href="/javascript">JavaScript</a></li>
+        <li><a href="/react">React</a></li>
+        <li><a href="/sanity">Sanity</a></li>
+        </ul>
+    </nav>
+        </div>
+    
+    );
 }
