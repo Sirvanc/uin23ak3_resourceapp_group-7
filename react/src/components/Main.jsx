1f060d9dfbe844e55c340198d5ad00aaa8cf4557
Sirvan Calabi <88987521+Sirvanc@users.noreply.github.com>
2024-02-23 02:21:35 +0100

    Update Main

--- react/src/components/Main.jsx
+++ react/src/components/Main.jsx
@@ -3,7 +3,16 @@ import React from "react";
 export default function Main() {
     return (
         <div className='ressouceBody'>
-            <h1>Ressursarkiv </h1>
+            <h2>Ressursarkiv </h2>
+            <nav>
+        <ul>
+        <li><a href="/html">HTML</a></li>
+        <li><a href="/css">CSS</a></li>
+        <li><a href="/javascript">JavaScript</a></li>
+        <li><a href="/react">React</a></li>
+        <li><a href="/sanity">Sanity</a></li>
+        </ul>
+    </nav>
         </div>
     
     );
