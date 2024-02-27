07f3a6cd4ecf4ef405163fd7d882cb67d3f295b6
Camilla H. Larsen <millalar@yahoo.no>
2024-02-27 17:04:50 +0100

    Endret oppsett
    Endret tilbake oppsettet med import av nav og main.
    Samt satt inn aktiv komponent til NAV og MAIN

--- react/src/components/Layout.jsx
+++ react/src/components/Layout.jsx
@@ -1,13 +1,16 @@
-import React from "react";
-import Nav from "./Nav";
-import Main from "./Main";
+import React from 'react';
+import Nav from './Nav';
+import Main from './Main';
 
-export default function Layout({ children }) {
+export default function Layout({}) {
+
+  
   return (
     <>
       <Nav />
+
       <Main />
-      {children}
-    </>
-  );
+      </>
+  )
 }
+
