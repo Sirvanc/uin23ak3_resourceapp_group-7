b240582ead886edb5ca4387b95abf54947bd1291
Sirvan Calabi <88987521+Sirvanc@users.noreply.github.com>
2024-02-27 14:13:28 +0100

    oppdatert

--- react/src/components/Layout.jsx
+++ react/src/components/Layout.jsx
@@ -7,11 +7,11 @@ export default function Layout({ children }) {
 
   
   return (
-
-      <main>
+    <>
         <Nav />
-        {children}
-      </main>
+      {children}
+      </>
+  
 
   );
 }
