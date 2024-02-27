d202b040fd8d67916eeaea53fe96af3fc77260da
Christian Albinsson <christian.k.albinsson@hiof.no>
2024-02-27 16:50:44 +0100

    Update Nav.jsx

--- react/src/components/Nav.jsx
+++ react/src/components/Nav.jsx
@@ -1,21 +1,29 @@
 import React from "react";
+import { useState } from "react";
+import { resources } from "../assets/ressurser.js";
+export { uniqueTags } from "../assets/ressurser.js";
 
-export default function Nav({ categories, onSelect }) {
+export default function Nav() {
+  const [activeTag, setActiveTag] = useState(null);
+
+  const uniqueTags = [];
+  resources.forEach((category) => {
+    if (uniqueTags.indexOf(category.category) === -1) {
+      uniqueTags.push(category.category);
+    }
+  });
+
   return (
-    <nav>
-      <ul>
-        {categories.map(category => (
-          <li key={category}>
-            <button onClick={() => onSelect(category)}>
-              {category.toUpperCase()}
-            </button>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <ul>
+      {uniqueTags.map((tag) => (
+        <button
+          key={tag}
+          className={activeTag === tag ? "active" : ""}
+          onClick={() => setActiveTag(tag)}
+        >
+          <a href={`#${tag}`}>{tag}</a>
+        </button>
+      ))}
+    </ul>
   );
 }
-
-/*Denne komponenten lager en enkel navigasjonsmeny med knapper for hver kategori som
-brukeren kan velge. Når en kategori blir valgt, kan en tilpasset funksjon bli kalt for å
-utføre relevante handlinger,for eksempel å oppdatere visningen av ressurser knyttet til den valgte kategorien.*/
