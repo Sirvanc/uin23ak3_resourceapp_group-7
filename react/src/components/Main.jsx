98f06f567f863687b540c6ceec25d7127b4a85fb
Christian Albinsson <christian.k.albinsson@hiof.no>
2024-02-27 17:36:52 +0100

    Nav update
    Fjernet css fra react.

--- react/src/components/Main.jsx
+++ react/src/components/Main.jsx
@@ -1,39 +1,10 @@
 import React from "react";
-import { resources } from "../assets/ressurser.js";
 
-export default function Main({ category }) {
-    const filteredResources = resources.filter(resource => resource.category === category);
-    const uppercaseCategory = category.toUpperCase(); // Konverter kategori til store bokstaver (uppercase)
-
-    return (
-        <main>
-            <article>
-                <div>
-                    {/* Rendre kategorinavnet med store bokstaver (uppercase) */}
-                    <h2>Resources {uppercaseCategory}</h2>
-                    
-                    <ul>
-                        {filteredResources.map(resource => (
-                            <li key={resource.id}>
-                                <a href={resource.url} target="_blank" rel="noopener noreferrer">
-                                    {resource.title}
-                                </a>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </article>
-        </main>
-    );
+export default function Main({ activeTag }) {
+  return (
+    <>
+      <h1>Ressursarkiv </h1>
+      {activeTag && <p>Active tag: {activeTag}</p>}
+    </>
+  );
 }
-/** Funksjonen Main definerer React-komponent Main  som tar imot category som en prop.
- * Inni funksjonen filtreres ressursene basert på den gitte kategorien.
- * Dette gjøres ved å bruke filter-metoden på resources-arrayen. 
- * Den filtrerte listen lagres i filteredResources.
- * Kategorien konverteres til store bokstaver ved hjelp av toUpperCase()-metoden, og resultatet lagres 
- * i variabelen uppercaseCategory.
- * Funksjonen Main defineres som en React-komponent som tar imot category som en prop.
- * Inni funksjonen filtreres ressursene basert på den gitte kategorien. 
- * Dette gjøres ved å bruke filter-metoden på resources-arrayen. Den filtrerte listen lagres i filteredResources.
- * Kategorien konverteres til store bokstaver ved hjelp av toUpperCase()-metoden,
- og resultatet lagres i variabelen uppercaseCategory. */
