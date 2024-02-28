6441409abf76f2f5e10b4bd28f37ad6eb2acc46a
Sirvan Calabi <88987521+Sirvanc@users.noreply.github.com>
2024-02-28 23:58:15 +0100

    Oppdatert

--- react/src/components/Layout.jsx
+++ react/src/components/Layout.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import Nav from './Nav';
-import Main from './Main';
+import React, { useState } from "react";
+import Nav from "./Nav";
+import Main from "./Main";
 
-export default function Layout({}) {
+export default function Layout() {
+  const categories = ["html", "css", "javascript", "react", "headless-cms"];
+  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
 
-  
+  const handleCategorySelect = category => {
+    setSelectedCategory(category);
+  };
+
   return (
-    <>
-      <Nav />
-      <Main/>
-
-
-      
-      </>
-  )
+    <div>
+      <Nav categories={categories} onSelect={handleCategorySelect} />
+      <Main category={selectedCategory} />
+    </div>
+  );
 }
-
