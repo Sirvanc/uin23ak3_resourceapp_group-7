429a22d3f965a5e7eacd4586ad928bdf3fd66be7
Sirvan Calabi <88987521+Sirvanc@users.noreply.github.com>
2024-02-28 02:35:26 +0100

    Siste  oppdatering
    Nav og Main , Layout  + Main Scss ble  oppdatert

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Main from './components/Main';
+import Nav from './components/Nav';
+
 
 
 function App() {
@@ -9,11 +11,8 @@ function App() {
     <Router>
       <Layout>
         <Routes>
-          <Route path="/HTML" element={<Main category="html" />} />
-          <Route path="/css" element={<Main category="css" />} />
-          <Route path="/javascript" element={<Main category="javascript" />} />
-          <Route path="/react" element={<Main category="react" />} />
-          <Route path="/sanity" element={<Main category="sanity" />} />
+          <Route path="/" element={<Nav />} />
+          <Route path="/:category" element={<Main />} />
         </Routes>
       </Layout>
     </Router>
