6d314a087fdb1c2327ec8f8beddeb39206c5255b
Sirvan Calabi <88987521+Sirvanc@users.noreply.github.com>
2024-02-28 02:35:26 +0100

    Siste  oppdatering
    Nav og Main , Layout  + Main Scss ble  oppdatert

--- react/src/components/Nav.jsx
+++ react/src/components/Nav.jsx
@@ -3,30 +3,28 @@ import { Link } from "react-router-dom";
 import { resources } from "../assets/ressurser.js";
 
 export default function Nav() {
-  const [activeTag, setActiveTag] = useState(null);
-
-  const uniqueTags = [];
-  resources.forEach((category) => {
-    if (uniqueTags.indexOf(category.category) === -1) {
-      uniqueTags.push(category.category);
-    }
-  });
-
   return (
-    <nav className="nav">
+  
+  <nav>
+    
       <ul>
-        {uniqueTags.map((tag) => (
-          <button
-            key={tag}
-            className={activeTag === tag ? "nav-button active" : "nav-button"}
-            onClick={() => setActiveTag(tag)}
-          >
-            <Link to={`/${tag}`} style={{ textTransform: "uppercase" }}>
-              {tag.toUpperCase()}
-            </Link>
-          </button>
-        ))}
+        <li>
+          <Link to="/html">HTML</Link>
+        </li>
+        <li>
+          <Link to="/css">CSS</Link>
+        </li>
+        <li>
+          <Link to="/javascript">JavaScript</Link>
+        </li>
+        <li>
+          <Link to="/react">React</Link>
+        </li>
+        <li>
+          <Link to="/sanity">Sanity</Link>
+        </li>
       </ul>
-    </nav>
+  </nav>
+  
   );
 }
