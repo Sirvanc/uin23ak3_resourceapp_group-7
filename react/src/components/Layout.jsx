8207d65c02eb3a8f3a99ae170cb6092b9d644123
Sirvan Calabi <88987521+Sirvanc@users.noreply.github.com>
2024-02-29 00:18:56 +0100

    Siste oppdateringer

--- react/src/components/Layout.jsx
+++ react/src/components/Layout.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Nav from "./Nav";
 import Main from "./Main";
 
+
 export default function Layout() {
   const categories = ["html", "css", "javascript", "react", "headless-cms"];
   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
