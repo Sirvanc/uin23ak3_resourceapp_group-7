1c4dbb2fc72ffc60af0215243efe7334053fdfe4
Sirvan Calabi <88987521+Sirvanc@users.noreply.github.com>
2024-02-28 23:58:15 +0100

    Oppdatert

--- react/src/components/Nav.jsx
+++ react/src/components/Nav.jsx
@@ -1,30 +1,17 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { resources } from "../assets/ressurser.js";
+import React from "react";
 
-export default function Nav() {
+export default function Nav({ categories, onSelect }) {
   return (
-  
-  <nav>
-    
+    <nav>
       <ul>
-        <li>
-          <Link to="/html">HTML</Link>
-        </li>
-        <li>
-          <Link to="/css">CSS</Link>
-        </li>
-        <li>
-          <Link to="/javascript">JavaScript</Link>
-        </li>
-        <li>
-          <Link to="/react">React</Link>
-        </li>
-        <li>
-          <Link to="/sanity">Sanity</Link>
-        </li>
+        {categories.map(category => (
+          <li key={category}>
+            <button onClick={() => onSelect(category)}>
+              {category.toUpperCase()}
+            </button>
+          </li>
+        ))}
       </ul>
-  </nav>
-  
+    </nav>
   );
 }
