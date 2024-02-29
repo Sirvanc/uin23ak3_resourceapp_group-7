500cf1a4ee7deef2badd89b4e3453f86b06d8285
Camilla H. Larsen <millalar@yahoo.no>
2024-02-29 21:54:02 +0100

    Oppdatert import-henvisning
    Endret mappe SCSS tilbake til STYLE. Oppdatert også henvisningen for app.jsx og main.jsx

---
+++ react/src/App.jsx
@@ -0,0 +1,28 @@
+/**Importsetninger */
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Main from './components/Main';
+import Nav from './components/Nav';
+import "./style/main.scss";
+
+
+
+function App() {
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Nav />} />
+          <Route path="/:category" element={<Main />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
+}
+
+export default App;
+
+/**Samlet sett setter denne koden opp en React-applikasjon med ruting ved hjelp 
+ * av React Router. Den har en layout-komponent som inneholder navigasjon og hovedinnhold, 
+ * og den bruker parametrisert ruting for å vise forskjellig innhold basert på den valgte kategorien. */
