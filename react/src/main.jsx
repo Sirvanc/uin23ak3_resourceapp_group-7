e3b1be62d78a6807001d80de6fdf767155b69de1
Camilla H. Larsen <millalar@yahoo.no>
2024-02-29 21:54:02 +0100

    Oppdatert import-henvisning
    Endret mappe SCSS tilbake til STYLE. Oppdatert også henvisningen for app.jsx og main.jsx

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import './scss/main.scss'
+import './style/main.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
