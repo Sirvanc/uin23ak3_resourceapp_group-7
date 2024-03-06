59f788a1a93ffbc7b98091fdcdff824f58c4db67
Sirvan Calabi <88987521+Sirvanc@users.noreply.github.com>
2024-03-06 22:29:15 +0100

    Update App.jsx

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,28 +1,19 @@
-/**Importsetninger */
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
-import Main from './components/Main';
-import Nav from './components/Nav';
-import "./style/main.scss";
+import { Routes, Route } from "react-router-dom"
+import Layout from "./components/Layout"
+import Main from "./components/Main"
 
 
 
 function App() {
+
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Nav />} />
-          <Route path="/:category" element={<Main />} />
-        </Routes>
-      </Layout>
-    </Router>
-  );
+    <Layout>
+      <Routes>
+        <Route path=":slug" element = "" />
+        <Route path=""/>
+      </Routes>
+    </Layout>
+  )
 }
 
-export default App;
-
-/**Samlet sett setter denne koden opp en React-applikasjon med ruting ved hjelp 
- * av React Router. Den har en layout-komponent som inneholder navigasjon og hovedinnhold, 
- * og den bruker parametrisert ruting for å vise forskjellig innhold basert på den valgte kategorien. */
+export default App
