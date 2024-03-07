959b7c2ccc649753518db34aa3246eda305f4e18
Sirvan Calabi <88987521+Sirvanc@users.noreply.github.com>
2024-03-07 07:25:19 +0100

    oppdatert

---
+++ react/src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { resources } from "../assets/reassures.js";
+
+export default function Main() {
+    return (
+        <div className='resourceBody'>
+            <h1>Ressursarkiv </h1>
+        </div>
+    
+    );
+}
