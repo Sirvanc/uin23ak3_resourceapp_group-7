4c37686563a78acf493ea05aa24503f55ee2a9a0
Sirvan Calabi <88987521+Sirvanc@users.noreply.github.com>
2024-03-07 08:16:26 +0100

    oppdatert

--- react/src/components/Main.jsx
+++ react/src/components/Main.jsx
@@ -1,11 +1,32 @@
-import React from "react";
-import { resources } from "../assets/reassures.js";
+import React, { useEffect, useState } from "react";
 
-export default function Main() {
-    return (
-        <div className='resourceBody'>
-            <h1>Ressursarkiv </h1>
-        </div>
+export default function Main(props) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
     
-    );
+    fetchData()
+      .then((result) => setData(result))
+      .catch((error) => console.error("Error fetching data: ", error));
+  }, []); 
+
+  const fetchData = async () => {
+    
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(["", "", ""]); 
+      }, 1000);
+    });
+  };
+
+  return (
+    <div>
+      <h1>Main Component</h1>
+      <ul>
+        {data.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
