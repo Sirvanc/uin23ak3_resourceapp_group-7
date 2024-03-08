8b85b131460d4bafce629d4cfd097906c627e553
Sirvan Calabi <88987521+Sirvanc@users.noreply.github.com>
2024-03-09 00:36:28 +0100

    Update Layout.jsx

--- react/src/components/Layout.jsx
+++ react/src/components/Layout.jsx
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import Main from "./Main";
 import Nav from "./Nav";
 
+
 export default function Layout() {
   const [activeTag, setActiveTag] = useState("html");
 
   return (
     <>
+
       <Nav activeTag={activeTag} setActiveTag={setActiveTag} />
       <Main activeTab={activeTag} />
     </>
