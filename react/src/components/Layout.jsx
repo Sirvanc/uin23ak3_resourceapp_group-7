c1f57387f5eff7fdce8214507f7938dffec3af96
Sirvan Calabi <88987521+Sirvanc@users.noreply.github.com>
2024-03-14 08:02:52 +0100

    Kommentarer til kodene

--- react/src/components/Layout.jsx
+++ react/src/components/Layout.jsx
@@ -17,4 +17,12 @@ export default function Layout({ children }) {
 return ( ... ): Dette er JSX som blir returnert av komponenten. 
 Det er et enkelt div-element med id-en "container". 
 Dette div-elementet utgjør hovedinnholdet i Layout-komponenten.
+div id="container">...</div>: Dette er en <div>-element med id-attributtet satt til 
+"container". All innholdet i Layout-komponenten er innenfor denne <div>-elementen.
+<Nav />: Dette er en referanse til Nav-komponenten som ble importert tidligere.
+ Denne komponenten vil rendres inn i Layout-komponenten på dette punktet.
+<main>{children}</main>: Dette er en <main>-element som inneholder children-innholdet.
+ children er det innholdet som er gitt som props til Layout-komponenten når den brukes. 
+ Dette tillater Layout-komponenten å fungere som en layout-container for annet innhold som 
+ skal rendres innenfor den.
  */
