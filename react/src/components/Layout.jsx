16f8472f0d9ca674b0ab55697a3a66acf23fc409
Christian Albinsson <christian.k.albinsson@hiof.no>
2024-03-18 14:14:36 +0100

    Revert "Merge branch 'layout' into new-test-delivert"
    This reverts commit 410e6e301e6ed068a14c5c657615ee5615e12eb7, reversing
    changes made to fb37888805f32a015b2d9ed9475d2ecf946d5f3b.

--- react/src/components/Layout.jsx
+++ react/src/components/Layout.jsx
@@ -1,28 +1,14 @@
 import React, { useState } from "react";
-import Nav from "./Nav";
 import Main from "./Main";
+import Nav from "./Nav";
 
-
-export default function Layout() {
-  const categories = ["html", "css", "javascript", "react", "headless-cms"];
-  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
-
-  const handleCategorySelect = category => {
-    setSelectedCategory(category);
-  };
+export default function Layout({ children }) {
+  const [activeTag, setActiveTag] = useState("html");
 
   return (
-    <div>
-      <Nav categories={categories} onSelect={handleCategorySelect} />
-      <Main category={selectedCategory} />
-    </div>
+    <>
+      <Nav activeTag={activeTag} setActiveTag={setActiveTag} />
+      <Main activeTab={activeTag}>{children}</Main>
+    </>
   );
 }
-/**Layout-komponenten binder sammen Nav og Main-komponentene og sørger for at 
- * de kommuniserer ved å sende kategorivalg fra Nav til Main og 
- * oppdatere visningen basert på den valgte kategorien. 
- * Ved hjelp av useState-hook'en, blir det definert en tilstand selectedCategory og en 
- * tilhørende funksjon setSelectedCategory. Den initielle verdien for selectedCategory 
- * settes til den første kategorien i categories-listen.handleCategorySelect-funksjonen blir definert 
- * for å oppdatere selectedCategory-tilstanden basert på den valgte kategorien. Denne funksjonen kalles når 
- * en kategori blir valgt i navigasjonsmenyen.*/
