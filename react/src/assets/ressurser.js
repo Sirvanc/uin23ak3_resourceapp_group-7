fdf61047014f51218ceff24ed7592d77ea3da549
Christian Albinsson <christian.k.albinsson@hiof.no>
2024-03-18 12:48:37 +0100

    Update ressurser.js
    Lagt in ressursers.js fra ab3

--- react/src/assets/ressurser.js
+++ react/src/assets/ressurser.js
@@ -1,96 +1,77 @@
 export const resources = [
     {
-        category: "HTML",
-        text: "HTML står for HyperText Markup Language, og er et strukturspråk som brukes for å lage strukturer til nettside- og applikasjonsgrensesnitt.",
-        sources: [
-            {
-                title: "W3Schools",
-                url: "https://www.w3schools.com/html/"
-            },
-            {
-                title: "HTML Living standard",
-                url: "https://html.spec.whatwg.org/multipage/"
-            },
-            {
-                title: "HTML.com Tutorials",
-                url: "https://html.com/"
-            },
-        ]
+        title: "W3Schools",
+        url: "https://www.w3schools.com/html/",
+        category: "html"
     },
     {
-        category: "CSS",
-        text: "CSS står for Cascading StyleSheets, og brukes for å sette stilregler på HTML-elementer.",
-        sources: [
-            {
-                title: "W3Schools",
-                url: "https://www.w3schools.com/css/"
-            },
-            {
-                title: "W3C HTML & CSS Standards",
-                url: "https://www.w3.org/standards/webdesign/htmlcss.html"
-            },
-            {
-                title: "W3C CSS Validator",
-                url: "https://jigsaw.w3.org/css-validator/"
-            },
-            {
-                title: "CSS Tricks",
-                url: "https://css-tricks.com/"
-            },
-        ]
+        title: "W3Schools",
+        url: "https://www.w3schools.com/css/",
+        category: "css"
     },
     {
-        category: "JavaScript",
-        text: "JavaScript er et scriptspråk basert på EcmaScript. JavaScript kjører direkte i nettleseren, og brukes ofte til å manipulere HTML og CSS i webgrensnesnitt.",
-        sources: [
-            {
-                title: "W3Schools",
-                url: "https://www.w3schools.com/js/"
-            },
-            {
-                title: "MDN Web Docs",
-                url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
-            },
-            {
-                title: "How to read JavaScript Documentation",
-                url: "https://www.youtube.com/watch?v=O3iR-CIufKM"
-            },
-        ]
+        title: "W3Schools",
+        url: "https://www.w3schools.com/js/",
+        category: "javascript"
     },
     {
-        category: "React",
-        text: "React er et rammeverk bygget i JavaScript. React bruker komponenter og states for å lage en levende frontend.",
-        sources: [
-            {
-                title: "React documentation",
-                url: "https://reactjs.org/docs/getting-started.html"
-            },
-            {
-                title: "W3Schools",
-                url: "https://www.w3schools.com/REACT/DEFAULT.ASP"
-            },
-            {
-                title: "How to read JavaScript Documentation",
-                url: "https://www.youtube.com/watch?v=O3iR-CIufKM"
-            },
-        ]
+        title: "W3Schools",
+        url: "https://www.w3schools.com/REACT/DEFAULT.ASP",
+        category: "react"
     },
     {
-        category: "Sanity and headless CMS",
-        text: "Sanity er et headless CMS som står for innholdsadministrasjon. Innhold hentes inn i applikasjoner via GROQ-spørringer.",
-        sources: [
-            {
-                title: "Sanity documentation",
-                url: "https://www.sanity.io/docs"
-            },
-            {
-                title: "OnCrawl: a beginners guide to headless CMS",
-                url: "https://www.oncrawl.com/technical-seo/beginners-guide-headless-cms/"
-            },
-            {
-                title: "Section.io: Getting started with Sanity CMS",
-                url: "https://www.section.io/engineering-education/getting-started-with-sanity-cms/"
-            },
-        ]
+        title: "HTML Living standard",
+        url: "https://html.spec.whatwg.org/multipage/",
+        category: "html"
     },
+    {
+        title: "HTML.com Tutorials",
+        url: "https://html.com/",
+        category: "html"
+    },
+    {
+        title: "W3C HTML & CSS Standards",
+        url: "https://www.w3.org/standards/webdesign/htmlcss.html",
+        category: "css"
+    },
+    {
+        title: "W3C CSS Validator",
+        url: "https://jigsaw.w3.org/css-validator/",
+        category: "css"
+    },
+    {
+        title: "CSS Tricks",
+        url: "https://css-tricks.com/",
+        category: "css"
+    },
+    {
+        title: "MDN Web Docs",
+        url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+        category: "javascript"
+    },
+    {
+        title: "How to read JavaScript Documentation",
+        url: "https://www.youtube.com/watch?v=O3iR-CIufKM",
+        category: "javascript"
+    },
+    {
+        title: "React documentation",
+        url: "https://reactjs.org/docs/getting-started.html",
+        category: "react"
+    },
+    {
+        title: "Sanity documentation",
+        url: "https://www.sanity.io/docs",
+        category: "headless-cms"
+    },
+    {
+        title: "OnCrawl: a beginners guide to headless CMS",
+        url: "https://www.oncrawl.com/technical-seo/beginners-guide-headless-cms/",
+        category: "headless-cms"
+    },
+    {
+        title: "Section.io: Getting started with Sanity CMS",
+        url: "https://www.section.io/engineering-education/getting-started-with-sanity-cms/",
+        category: "headless-cms"
+    }
 ]
