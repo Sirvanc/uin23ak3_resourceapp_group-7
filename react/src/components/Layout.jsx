f5a9e9bf5fcc2631ca59abdfc9e287f0c89118ab
Christian Albinsson <christian.k.albinsson@hiof.no>
2024-03-18 13:19:32 +0100

    test av main

--- react/src/components/Layout.jsx
+++ react/src/components/Layout.jsx
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import Main from "./Main";
 import Nav from "./Nav";
 
-export default function Layout() {
+export default function Layout({ children }) {
   const [activeTag, setActiveTag] = useState("html");
+  console.log("layout", activeTag);
 
   return (
     <>
       <Nav activeTag={activeTag} setActiveTag={setActiveTag} />
-      <Main activeTab={activeTag} />
+      <Main activeTab={activeTag}>{children}</Main>
     </>
   );
 }
