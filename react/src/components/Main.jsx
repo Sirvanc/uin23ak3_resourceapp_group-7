95c27dcaec87e37bea281c0686225df4605156af
Christian Albinsson <christian.k.albinsson@hiof.no>
2024-03-19 14:00:44 +0100

    Update Main.jsx
    Fjernet code som ikke er brukt

--- react/src/components/Main.jsx
+++ react/src/components/Main.jsx
@@ -1,13 +1,7 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"; 
 import { resources } from "../assets/ressurser.js";
 
 export default function Resources(props) {
-  const { activeTab, category } = props;
-  console.log("main", activeTab, category);
-
-  const { slug } = useParams();
-  const [resource, setResource] = useState();
+  const { activeTab } = props;
 
   return (
     <section>
