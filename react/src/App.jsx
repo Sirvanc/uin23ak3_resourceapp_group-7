f0ab407e639448bfea7bca0257aafb60dd0ce005
Camilla H. Larsen <millalar@yahoo.no>
2024-03-19 13:57:43 +0100

    Endring mtp sletting av resource.jsx
    Fjernet i APP.jsx: importen til resource.jsx og Route som vi ikke skal bruke.

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
 import { resources } from './assets/ressurser'
-import Resources from './components/Resources';
 import Layout from './components/Layout';
 
 function App() { 
@@ -11,12 +10,7 @@ function App() {
     <Layout>
       <Routes>
         
-        <Route path='/' element={<Navigate to='/HTML' />} /> /**  */
-        <Route path='/HTML' element={<Resources resources={resources} category={"html"} />} />
-        <Route path='/CSS' element={<Resources resources={resources} category={"css"} />} />
-        <Route path='/JavaScript' element={<Resources resources={resources} category={"JavaScript"} />} />
-        <Route path='/React' element={<Resources resources={resources} category={"React"} />} />
-        <Route path='/Sanity-and-headless-CMS' element={<Resources resources={resources} category={"Sanity and headless CMS"} />} />
+        <Route path='/' element={<Navigate to='/HTML' />} />
         
       </Routes>
     </Layout>
