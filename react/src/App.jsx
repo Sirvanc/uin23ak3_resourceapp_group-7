41cd6b6800f9c3eef683120c2b98dad010cfd6ff
Christian Albinsson <christian.k.albinsson@hiof.no>
2024-03-19 14:20:38 +0100

    Update App.jsx

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,17 +1,13 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { Navigate } from 'react-router-dom';
-import { resources } from './assets/ressurser'
-import Layout from './components/Layout';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import Layout from "./components/Layout";
 
-function App() { 
-
-  return ( 
+function App() {
+  return (
     <Layout>
       <Routes>
-        
-        <Route path='/' element={<Navigate to='/HTML' />} />
-        
+        <Route path="" element={<Layout />} />
       </Routes>
     </Layout>
   );
