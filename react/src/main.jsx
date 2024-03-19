5b4259a2181c15f73775ddcd821fbaae4ebda110
Camilla H. Larsen <millalar@yahoo.no>
2024-03-19 14:02:21 +0100

    SRC/MAIN.jsx
    små endringer i strukturen

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -2,15 +2,13 @@ import './index.css'
 import './styles/main.scss'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Route } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
-  <React.StrictMode>
-    <Route>
+    <Router>
       <App />
-    </Route>
-  </React.StrictMode>,
+    </Router>
+ 
   
 )
